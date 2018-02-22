3a88fdc249b8bcb955525498e0056bb5af446ef8
Christian Weiss <worldmover777@gmail.com>
2018-02-22 04:27:08 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 # code your #valid_move? method here
 def valid_move(board,move)
-  if move<0 || move>8 || board[move]!=" "
+  if move<0 || move>8 || position_taken[move]
     return false
   end
 else
@@ -10,3 +10,11 @@ end
 
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken(board,move)
+  if board[move]!=" "
+    return false
+  end
+else
+  return true
+end
+end
